c246a46f016f9ce47a18a064e9086e03da2e2650
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-03-06 15:43:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
+const textos + document.querySElectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+for(let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j<botoes.length;j++){
+        for(let j = 0; j < botoes.length ;j++) {
             botoes[j].classList.remove("ativo");
         }
 
