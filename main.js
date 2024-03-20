98fb84ba446ddca3fafff6a7ed11d5837248df99
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-03-20 15:42:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,9 @@ function atualizaCronometro(){
         document.getElementById("dias"++i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas"++i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min"++i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"++i).textContent = calculaTempo(tempos[i])[3];   
+        document.getElementById("seg"++i).textContent = calculaTempo(tempos[i])[3]; 
+        for (let i=0; i<contadores.length;i++){
+            // contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
 
